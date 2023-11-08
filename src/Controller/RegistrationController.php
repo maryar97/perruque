73cfa6849a6cf8901568dd3241bfee0d025862a0
maryar97 <marydiva201170@gmail.fr>
2023-11-08 13:38:32 +0100

    notif

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -35,38 +35,10 @@ class RegistrationController extends AbstractController
                 )
             );
 
-            $user->setCreatedAt(new \DateTimeImmutable());
 
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
-
-            // on génère le JWT de l'utilisation 
-            // on crée Header 
-            $header = [
-                'typ' => 'JWT',
-                'alg' => 'HS256'
-            ];
-
-            // on crée le payload
-            $payload = [
-                'user_id' => $user->getId()
-            ];
-
-            // on génère le token 
-            $token = $jwt->generate($header, $payload,
-            $this->getParameter('app.jwtsecret'));
-
-            
-
-            // on envoie un mail
-            $mail->send(
-                'no-reply@monsite.net',
-                $user->getEmail(),
-                'Activation de votre compte sur le site e-commerce',
-                'register',
-                compact('user', 'token')
-                );
 
             return $userAuthenticator->authenticateUser(
                 $user,
@@ -80,77 +52,4 @@ class RegistrationController extends AbstractController
         ]);
     }
 
-    #[Route('/verif/{token}', name: 'verify_user')]
-    public function verifyUser($token, JWTService $jwt, UsersRepository $usersRepository, EntityManagerInterface $em): Response 
-    {
-        // on verifie si le token est valide, n'a pas expiré et n'a pas été modifié 
-        if($jwt->isValid($token) && !$jwt->isExpired($token) && $jwt->check($token, $this->getParameter('app.jwtsecret')))
-        {
-            // on récupère le payload 
-            $payload = $jwt->getPayload($token);
-
-            // on récupère le user du token 
-            $user = $usersRepository->find($payload['user_id']); 
-
-            // on vérifie que l'utilisateur existe et n' a pas encore activé son compte 
-            if($user && !$user->getIsVerified()){
-                $user->setIsVerified(true); 
-                $em->flush($user);
-                $this->addFlash('success', 'Utilisateur activé');
-                return $this->redirectToRoute('profile_index');
-            }
-        
         }
-          // Ici un probleme se pose dans le token 
-          $this->addFlash('danger', 'Le token est invalide ou a expiré');
-          return $this->redirectToRoute('app_login');
-    }
-
-    #[Route('/renvoiverif', name: 'resend_verif')]
-    public function resendVerif(JWTService $jwt, SendMailService $mail, UsersRepository $usersRepository): Response 
-    {
-
-        $user = $this->getUser();
-
-        if(!$user){
-            $this->addFlash('danger', 'Vous devez être connecté pour accéder à cette page');
-            return $this->redirectToRoute('app_login');
-        }
-
-        if ($user->getIsVerified()){
-            $this->addFlash('warning', 'Cet utilisateur est déjà activé');
-            return $this->redirectToRoute('profile_index');
-
-        }
-
-            // on génère le JWT de l'utilisation 
-            // on crée Header 
-            $header = [
-                'typ' => 'JWT',
-                'alg' => 'HS256'
-            ];
-
-            // on crée le payload
-            $payload = [
-                'user_id' => $user->getId()
-            ];
-
-            // on génère le token 
-            $token = $jwt->generate($header, $payload,
-            $this->getParameter('app.jwtsecret'));
-
-            
-
-            // on envoie un mail
-            $mail->send(
-                'no-reply@monsite.net',
-                $user->getEmail(),
-                'Activation de votre compte sur le site e-commerce',
-                'register',
-                compact('user', 'token')
-                );
-
-                $this->addFlash('success', 'Email de vérification envoyé');
-                return $this->redirectToRoute('profile_index');
-    }
-}
